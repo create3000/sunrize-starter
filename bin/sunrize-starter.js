93a827f18b1af1e990cf0480d5a2523c79a5ded8
Holger Seelig <holger.seelig@gmail.com>
2023-12-15 10:11:40 +0100

    Added files.

--- bin/sunrize-starter.js
+++ bin/sunrize-starter.js
@@ -1,14 +1,12 @@
 #!/usr/bin/env node
 "use strict";
 
-const os = require ("os");
 const path = require ("path");
 const { spawn } = require ("child_process");
-const cmd = os .platform () === "win32" ? "npm.cmd" : "npm";
 
 process .chdir (path .resolve (__dirname, ".."));
 
-const p = spawn (cmd, ["start", "--silent", "--", ... process .argv .slice (2)]);
+const p = spawn ("npx", ["--yes", "sunrize"]);
 
 p .stdout .pipe (process .stdout);
 p .stderr .pipe (process .stderr);
